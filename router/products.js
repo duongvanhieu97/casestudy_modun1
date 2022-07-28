b09bc09b06c21f4e7cff836f2fd16b48a3c6a5ba
Duonghieu97st <Duonghieu97st@gmail.com>
2022-07-28 23:12:15 +0700

    dua len git

---
+++ router/products.js
@@ -0,0 +1,46 @@
+class Products {
+    name;
+    price;
+    category;
+    image;
+
+    constructor(name, price, category, image) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.image = image
+    };
+
+    getName() {
+        return this.name
+    };
+
+    getPrice() {
+        return this.price
+    };
+
+    getCategory() {
+        return this.category
+    };
+
+    getImage() {
+        return this.image
+    };
+
+
+    setName(name) {
+        this.name = name;
+    }
+
+    setPrice(price) {
+        this.price = price;
+    }
+
+    setImage(image) {
+        this.image = image;
+    }
+
+    setCategory(category) {
+        this.category = category;
+    }
+}
