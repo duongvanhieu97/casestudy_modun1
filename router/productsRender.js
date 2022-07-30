9c502651c7c4d38944c7561307a1f48fd17d8362
Duonghieu97st <Duonghieu97st@gmail.com>
2022-07-30 19:00:42 +0700

    update products

--- router/productsRender.js
+++ router/productsRender.js
@@ -1,42 +1,42 @@
 //Lấy tên sản phẩm với hình ảnh với giá với danh sách nhóm sản phẩm
 //danh sách sản phẩm đồng hồ
-new Products('Đồng Hồ Nam Casio MTP-1374D-1AV Màu Trắng','1.850.000','Đồng hồ', "(../image/oclock1.jpg)")
-let oclock1 = new Products('Đồng Hồ Nam Casio MTP-1374D-1AV Màu Trắng','1.850.000','Đồng hồ', "(../image/oclock1.jpg)")
-let oclock2 = new Products('Đồng Hồ Casio Nam MTP-1374L-1AVDF Màu Đen','1.850.000','Đồng hồ', "(../image/oclock2.jpg)")
-let oclock3 = new Products('Đồng Hồ Bulova Swarovski Crystals Rose Gold Tone Watch 98L261','3.750.000','Đồng hồ', "(../image/oclock3.jpg)")
-let oclock4 = new Products('Đồng Hồ Nữ Julius JA-1040A Màu Vàng','793.000','Đồng hồ', "(../image/oclock4.jpg)")
-let oclock5 = new Products('Đồng Hồ Nam Salvatore Ferragamo 1898 SFFM01320 Màu Bạc','16.600.000','Đồng hồ', "(../image/oclock5.jpg)")
-let oclock6 = new Products('Đồng Hồ Nữ Armitron Women\'s Genuine Crystal Accented Bracelet Watch 75/5747 Màu Bạc','2.300.000','Đồng hồ', "(../image/oclock6.jpg)")
+new Products('Đồng Hồ Nam Casio MTP-1374D-1AV Màu Trắng','1.850.000','Đồng hồ', "/image/oclock1.jpg")
+let oclock1 = new Products('Đồng Hồ Nam Casio MTP-1374D-1AV Màu Trắng','1.850.000','Đồng hồ', "/image/oclock1.jpg")
+let oclock2 = new Products('Đồng Hồ Casio Nam MTP-1374L-1AVDF Màu Đen','1.850.000','Đồng hồ', "/image/oclock2.jpg")
+let oclock3 = new Products('Đồng Hồ Bulova Swarovski Crystals Rose Gold Tone Watch 98L261','3.750.000','Đồng hồ', "/image/oclock3.jpg")
+let oclock4 = new Products('Đồng Hồ Nữ Julius JA-1040A Màu Vàng','793.000','Đồng hồ', "/image/oclock4.jpg")
+let oclock5 = new Products('Đồng Hồ Nam Salvatore Ferragamo 1898 SFFM01320 Màu Bạc','16.600.000','Đồng hồ', "/image/oclock5.jpg")
+let oclock6 = new Products('Đồng Hồ Nữ Armitron Women\'s Genuine Crystal Accented Bracelet Watch 75/5747 Màu Bạc','2.300.000','Đồng hồ', "/image/oclock6.jpg")
 let arrClock = [[oclock1,oclock2,oclock3], [oclock4,oclock5,oclock6]]
 
 //danh sách sản phẩm mỹ phẩm
-new Products('Toner BHA Obagi Clenziderm MD Pore Therapy 2% 148ml, Giảm Mụn Nhờn','626.000','Mỹ phẩm', "(../image/cosmetic1.jpg)")
-let cosmetic1= new Products('Toner BHA Obagi Clenziderm MD Pore Therapy 2% 148ml, Giảm Mụn Nhờn','626.000','Mỹ phẩm', "(../image/cosmetic1.jpg)")
-let cosmetic2 = new Products('Kem Hỗ Trợ Trẻ Hóa Da Và Hỗ Trợ Tái Tạo Da Obagi 360 Retinol 0.5%','998.000','Mỹ phẩm', "(../image/cosmetic2.jpg)")
-let cosmetic3 = new Products('Kem Dưỡng Hỗ Trợ Da Trẻ Hóa, Hỗ Trợ Làm Giảm Mụn Obagi 360 1.0% 28g','1.189.000','Mỹ phẩm', "(../image/cosmetic3.jpg)")
-let cosmetic4 = new Products('Serum Dưỡng Da Goodndoc Hydra B5 30ml','495.000','Mỹ phẩm', "(../image/cosmetic4.jpg)")
-let cosmetic5 = new Products('Kem Chống Nắng Mesoestetic Mesoprotech® Mineral Matt Antiaging Fluid 50ml','1.360.000','Mỹ phẩm', "(../image/cosmetic5.jpg)")
-let cosmetic6 = new Products('Tinh Chất Hỗ Trợ Thay Da Sinh Học BioRePeelCl3 FND (6 x 6ml) Hộp 6 Lọ','4.990.000','Mỹ phẩm', "(../image/cosmetic6.jpg)")
+new Products('Toner BHA Obagi Clenziderm MD Pore Therapy 2% 148ml, Giảm Mụn Nhờn','626.000','Mỹ phẩm', "/image/cosmetic.jpg")
+let cosmetic1= new Products('Toner BHA Obagi Clenziderm MD Pore Therapy 2% 148ml, Giảm Mụn Nhờn','626.000','Mỹ phẩm', "/image/cosmetic.jpg")
+let cosmetic2 = new Products('Kem Hỗ Trợ Trẻ Hóa Da Và Hỗ Trợ Tái Tạo Da Obagi 360 Retinol 0.5%','998.000','Mỹ phẩm', "/image/cosmetic2.jpg")
+let cosmetic3 = new Products('Kem Dưỡng Hỗ Trợ Da Trẻ Hóa, Hỗ Trợ Làm Giảm Mụn Obagi 360 1.0% 28g','1.189.000','Mỹ phẩm', "/image/cosmetic3.jpg")
+let cosmetic4 = new Products('Serum Dưỡng Da Goodndoc Hydra B5 30ml','495.000','Mỹ phẩm', "/image/cosmetic4.jpg")
+let cosmetic5 = new Products('Kem Chống Nắng Mesoestetic Mesoprotech® Mineral Matt Antiaging Fluid 50ml','1.360.000','Mỹ phẩm', "/image/cosmetic5.jpg")
+let cosmetic6 = new Products('Tinh Chất Hỗ Trợ Thay Da Sinh Học BioRePeelCl3 FND (6 x 6ml) Hộp 6 Lọ','4.990.000','Mỹ phẩm', "/image/cosmetic6.jpg")
 let arrCosmetic = [[cosmetic1,cosmetic2,cosmetic3], [cosmetic4,cosmetic5,cosmetic6]]
 
 //danh sách sản phẩm nước hoa
-new Products('Nước Hoa Nam Nautica Voyage EDT 100ml','790.000','Nước hoa', "(../image/perfume1.jpg)")
-let perfume1= new Products('Nước Hoa Nam Nautica Voyage EDT 100ml','790.000','Nước hoa', "(../image/perfume1.jpg)")
-let perfume2 = new Products('Nước Hoa Nam Armaf Club De Nuit Intense For Man, 105ml','990.000','Nước hoa', "(../image/perfume2.jpg)")
-let perfume3 = new Products('Nước Hoa Nam Versace Eros Man EDT 5ml','250.000','Nước hoa', "(../image/perfume3.jpg)")
-let perfume4 = new Products('Nước Hoa Nữ Armaf Club De Nuit EDP 105ml','890.000','Nước hoa', "(../image/perfume4.jpg)")
-let perfume5 = new Products('Set Nước Hoa Nữ Và Sữa Dưỡng Thể Paco Rabanne Olympéa Gift Set (EDP 80ml + Body Lotion 100ml)','2.550.000','Nước hoa', "(../image/perfume5.jpg)")
-let perfume6 = new Products('Set Nước Hoa Nữ Và Sữa Dưỡng Thể Jimmy Choo (EDP 100ml + EDP 7.5ml + Body Lotion 100ml)','2.450.000','Nước hoa', "(../image/perfume6.jpg)")
+new Products('Nước Hoa Nam Nautica Voyage EDT 100ml','790.000','Nước hoa', "/image/perfume1.jpg")
+let perfume1= new Products('Nước Hoa Nam Nautica Voyage EDT 100ml','790.000','Nước hoa', "/image/perfume1.jpg")
+let perfume2 = new Products('Nước Hoa Nam Armaf Club De Nuit Intense For Man, 105ml','990.000','Nước hoa', "/image/perfume2.jpg")
+let perfume3 = new Products('Nước Hoa Nam Versace Eros Man EDT 5ml','250.000','Nước hoa', "/image/perfume3.jpg")
+let perfume4 = new Products('Nước Hoa Nữ Armaf Club De Nuit EDP 105ml','890.000','Nước hoa', "/image/perfume4.jpg")
+let perfume5 = new Products('Set Nước Hoa Nữ Và Sữa Dưỡng Thể Paco Rabanne Olympéa Gift Set (EDP 80ml + Body Lotion 100ml)','2.550.000','Nước hoa', "/image/perfume5.jpg")
+let perfume6 = new Products('Set Nước Hoa Nữ Và Sữa Dưỡng Thể Jimmy Choo (EDP 100ml + EDP 7.5ml + Body Lotion 100ml)','2.450.000','Nước hoa', "/image/perfume6.jpg")
 let arrPerfume = [[perfume1,perfume2,perfume3], [perfume4,perfume5,perfume6]]
 
 //danh sách sản phẩm son môi
-new Products('Son Dưỡng Dior Addict Lip Glow Màu 004 Coral (Mới Nhất 2021)','769.000','Son môi', "(../image/lipstick1.jpg)")
-let lipstick1= new Products('Son Dưỡng Dior Addict Lip Glow Màu 004 Coral (Mới Nhất 2021)','769.000','Son môi', "(../image/lipstick1.jpg)")
-let lipstick2 = new Products('Son MAC Ruby Woo - Sắc Đỏ Siêu Lì Cổ Điển','470.000','Son môi', "(../image/lipstick2.jpg)")
-let lipstick3 = new Products('Son Dior Rouge Velvet Màu 999 Màu Đỏ Tươi Mini','475.000','Son môi', "(../image/lipstick3.jpg)")
-let lipstick4 = new Products('Son MAC Powder Kiss Lipstick Màu 916 Devoted To Chili Màu Đỏ Gạch','480.000','Son môi', "(../image/lipstick4.jpg)")
-let lipstick5 = new Products('Son Kem Shu Uemura Kinu RD 192 Màu Đỏ Mận','689.000','Son môi', "(../image/lipstick5.jpg)")
-let lipstick6 = new Products('Son Dior Rouge Dior Forever Transfer-Proof Lipstick 742 - Forever Sisterhood Màu Đỏ Tươi','849.000','Son môi', "(../image/lipstick6.jpg)")
+new Products('Son Dưỡng Dior Addict Lip Glow Màu 004 Coral (Mới Nhất 2021)','769.000','Son môi', "/image/lipstick1.jpg")
+let lipstick1= new Products('Son Dưỡng Dior Addict Lip Glow Màu 004 Coral (Mới Nhất 2021)','769.000','Son môi', "/image/lipstick1.jpg")
+let lipstick2 = new Products('Son MAC Ruby Woo - Sắc Đỏ Siêu Lì Cổ Điển','470.000','Son môi', "/image/lipstick2.png")
+let lipstick3 = new Products('Son Dior Rouge Velvet Màu 999 Màu Đỏ Tươi Mini','475.000','Son môi', "/image/lipstick3.jpg")
+let lipstick4 = new Products('Son MAC Powder Kiss Lipstick Màu 916 Devoted To Chili Màu Đỏ Gạch','480.000','Son môi', "/image/lipstick4.jpg")
+let lipstick5 = new Products('Son Kem Shu Uemura Kinu RD 192 Màu Đỏ Mận','689.000','Son môi', "/image/lipstick5.jpg")
+let lipstick6 = new Products('Son Dior Rouge Dior Forever Transfer-Proof Lipstick 742 - Forever Sisterhood Màu Đỏ Tươi','849.000','Son môi', "/image/lipstick6.jpg")
 let arrLipstick = [[lipstick1,lipstick2,lipstick3], [lipstick4,lipstick5,lipstick6]]
 
 //Hiển thị sản phẩm
@@ -50,10 +50,11 @@ function showProducts(arr) {
             content += "<h2>" + arr[i][j].name + "</h2>"
             content += "<p>Giá: " + arr[i][j].price + " đ</p>"
             //Xem đã đăng nhập chưa nếu chưa đăng nhập thì không hỉển thị phần xóa sửa sản phẩm
-            if (!checkLogin()) {
-                content += "<button onclick='updateProducts(" + i + "," + j + ',\"' + arr[i][j].getCategory() + '"' + ")'>&nbsp;Sửa&nbsp;</button>" + "&emsp;"
-                content += "<button onclick='deleteProduct(" + i + "," + j + ',\"' + arr[i][j].getCategory() + '"' + ")'>&nbsp;Xóa&nbsp;</button>"
-            }
+            //chưa sửa được lôi này
+            // if (!checkLogin()) {
+            //     content += "<button onclick='updateProducts(" + i + "," + j + ',\"' + arr[i][j].getCategory() + '"' + ")'>&nbsp;Sửa&nbsp;</button>" + "&emsp;"
+            //     content += "<button onclick='deleteProduct(" + i + "," + j + ',\"' + arr[i][j].getCategory() + '"' + ")'>&nbsp;Xóa&nbsp;</button>"
+            // }
             content += "</td>"
         }
         content += "</tr>"
